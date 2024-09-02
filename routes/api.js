d0860d4db718c5c29e6cf4db126d76ed301df653
shivam1739 <patelshivam1739@gmail.com>
2024-09-02 17:52:39 +0530

    role added in token

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,17 @@
 import express from 'express';
-import authRouter from './v1/auth.routes.js';
-import roleRoute from './v1/role.routes.js';
-import eventRoute from './v1/event.route.js';
-import ticketRoute from './v1/ticket.routes.js';
+import authRouter from './auth.routes.js';
+import roleRoute from './role.routes.js';
+import eventRoute from './event.route.js';
+import ticketRoute from './ticket.routes.js';
 
 
 const router = express.Router();
 
 
-router.use('/v1/auth', authRouter);
-router.use('/v1/roles', roleRoute);
-router.use('/v1/event', eventRoute);
-router.use('/v1/ticket', ticketRoute);
+router.use('/auth', authRouter);
+router.use('/roles', roleRoute);
+router.use('/event', eventRoute);
+router.use('/ticket', ticketRoute);
 
 
 
